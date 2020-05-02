5b26a414686ba0487321e74ced6e58efe58a5861
felippegaede <felippe.gaede@hotmail.com>
2020-05-02 11:45:16 -0300

    Criando a classe PageAdin

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
+
+
 
 $app->get('/', function() {
     
@@ -15,6 +18,14 @@ $page = new Page();
 $page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+	$page->setTpl("index");
+	
+	});
+
 
 
 
