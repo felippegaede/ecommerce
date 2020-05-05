178d428fc42d8a9077ddd87bb2724e0c6aeb8807
felippegaede <felippe.gaede@hotmail.com>
2020-05-04 23:18:00 -0300

    Site categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -19,6 +19,7 @@ class Category extends Model
 
    public function save()
    {
+
     $sql = new Sql;
 
     $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
@@ -30,10 +31,13 @@ class Category extends Model
 
      $this->setData($results[0]);
 
+     Category::updateFile();
+
    }
 
    public function get ($idcategory)
    {
+
        $sql = new Sql;
 
        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", array(
@@ -46,11 +50,32 @@ class Category extends Model
 
    public function delete()
    {
+
     $sql = new Sql;
 
     $sql->query("DELETE  FROM tb_categories WHERE idcategory = :idcategory", array(
         ":idcategory" => $this->getidcategory()
     ));
 
+    Category::updateFile();
+
    }
+
+   public static function updateFile()
+   {
+
+        $categories = Category::listAll();
+
+        $html = array();
+
+        foreach ($categories as $row)
+        {
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'
+            .$row['descategory'].'</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR.
+        "categories-menu.html", implode("" , $html));
+   }
+
 }
---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Apple </a></li><li><a href="/categories/3">LG</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/5">Samsung</a></li>
